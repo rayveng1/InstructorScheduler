b81c857ce73f6f488924d2a9d3f8b29fbfadaa0f
Dongji Yang <dongji.yang@outlook.com>
2023-10-20 15:27:18 -0500

    init

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,3 @@
+export default function BaseDashboardPage() {
+  return <p>HELLO</p>;
+}
---
+++ src/app/dashboard/settings/page.tsx
@@ -0,0 +1,317 @@
+"use client";
+
+export default function SettingsPage() {
+  return (
+    <div className="container mx-auto my-5 p-5">
+      <div className="md:flex no-wrap md:-mx-2 ">
+        {/* <!-- Left Side --> */}
+        <div className="w-full md:w-3/12 md:mx-2">
+          {/* <!-- Profile Card --> */}
+          <div className="bg-white dark:bg-slate-800 p-3 border-t-4 border-blue-500">
+            <div className="image overflow-hidden">
+              <img
+                className="h-[200px] w-[200px] mx-auto rounded-full"
+                src="https://placekitten.com/200/200"
+                alt=""
+              />
+            </div>
+            <h1 className="text-gray-900 dark:text-white font-bold text-xl leading-8 my-1">
+              Jane Doe
+            </h1>
+            <h3 className="text-gray-600 dark:text-white font-lg text-semibold leading-6">
+              Assist. Prof. of Instruction - Computer Science
+            </h3>
+            <p className="text-sm text-gray-500 dark:text-gray-300 leading-6">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Reprehenderit, eligendi dolorum sequi illum qui unde aspernatur
+              non deserunt
+            </p>
+            <ul className="bg-gray-100 dark:bg-slate-700 dark:text-white text-gray-600 hover:shadow py-2 px-3 mt-3 divide-y rounded shadow-sm">
+              <li className="flex items-center py-3">
+                <span>Status</span>
+                <span className="ml-auto">
+                  <span className="bg-blue-500 py-1 px-2 rounded text-white text-sm">
+                    Active
+                  </span>
+                </span>
+              </li>
+              <li className="flex items-center py-3">
+                <span>Member since</span>
+                <span className="ml-auto">Nov 07, 2016</span>
+              </li>
+            </ul>
+          </div>
+          {/* <!-- End of profile card --> */}
+          <div className="my-4"></div>
+        </div>
+        {/* <!-- Right Side --> */}
+        <div className="w-full md:w-9/12 mx-2 h-64">
+          {/* <!-- Profile tab --> */}
+          {/* <!-- About Section --> */}
+          <div className="bg-white dark:bg-slate-800 p-3 shadow-sm rounded-sm">
+            <div className="flex items-center space-x-2 font-semibold text-gray-900 dark:text-white leading-8">
+              <span className="text-blue-500">
+                <svg
+                  className="h-5"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+                  />
+                </svg>
+              </span>
+              <span className="tracking-wide">Profile</span>
+            </div>
+            <div className="text-gray-700 dark:text-white">
+              <div className="grid md:grid-cols-2 text-sm">
+                <div className="grid grid-cols-2">
+                  <div className="px-4 py-2 font-semibold">First Name</div>
+                  <div className="px-4 py-2">Jane</div>
+                </div>
+                <div className="grid grid-cols-2">
+                  <div className="px-4 py-2 font-semibold">Last Name</div>
+                  <div className="px-4 py-2">Doe</div>
+                </div>
+                <div className="grid grid-cols-2">
+                  <div className="px-4 py-2 font-semibold">Gender</div>
+                  <div className="px-4 py-2">Female</div>
+                </div>
+                <div className="grid grid-cols-2">
+                  <div className="px-4 py-2 font-semibold">Phone</div>
+                  <div className="px-4 py-2">(555) 555-5555</div>
+                </div>
+                <div className="grid grid-cols-2">
+                  <div className="px-4 py-2 font-semibold">Address</div>
+                  <div className="px-4 py-2">Beech Creek, PA, Pennsylvania</div>
+                </div>
+
+                <div className="grid grid-cols-2">
+                  <div className="px-4 py-2 font-semibold">Email.</div>
+                  <div className="px-4 py-2">
+                    <a className="text-blue-800" href="mailto:jane@example.com">
+                      jane@example.com
+                    </a>
+                  </div>
+                </div>
+                <div className="grid grid-cols-2">
+                  <div className="px-4 py-2 font-semibold">Birthday</div>
+                  <div className="px-4 py-2">Feb 06, 1998</div>
+                </div>
+              </div>
+            </div>
+            <button className="block w-full text-blue-800 dark:text-blue-300 text-sm font-semibold rounded-lg hover:bg-gray-100 focus:outline-none focus:shadow-outline focus:bg-gray-100 hover:shadow-xs p-3 my-4">
+              <span className="w-max flex mx-auto items-center justify-center gap-x-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+                  />
+                </svg>
+                Edit Information
+              </span>
+            </button>
+          </div>
+          {/* <!-- End of about section --> */}
+
+          <div className="my-4"></div>
+
+          {/* <!-- Experience and education --> */}
+          <div className="bg-white dark:bg-slate-800 p-3 pb-8 shadow-sm rounded-sm">
+            <div className="grid grid-cols-2">
+              <div>
+                <div className="flex items-center justify-between w-2/3 space-x-2 font-semibold dark:text-white text-gray-900 leading-8 mb-3">
+                  <div className="flex items-center gap-x-2">
+                    <span className="text-blue-500">
+                      <svg
+                        className="h-5"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
+                        />
+                      </svg>
+                    </span>
+                    <span className="tracking-wide">Experience</span>
+                  </div>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="w-5 h-5"
+                  >
+                    <path d="M5.433 13.917l1.262-3.155A4 4 0 017.58 9.42l6.92-6.918a2.121 2.121 0 013 3l-6.92 6.918c-.383.383-.84.685-1.343.886l-3.154 1.262a.5.5 0 01-.65-.65z" />
+                    <path d="M3.5 5.75c0-.69.56-1.25 1.25-1.25H10A.75.75 0 0010 3H4.75A2.75 2.75 0 002 5.75v9.5A2.75 2.75 0 004.75 18h9.5A2.75 2.75 0 0017 15.25V10a.75.75 0 00-1.5 0v5.25c0 .69-.56 1.25-1.25 1.25h-9.5c-.69 0-1.25-.56-1.25-1.25v-9.5z" />
+                  </svg>
+                </div>
+                <ul className="list-inside space-y-2">
+                  <li>
+                    <div className="text-teal-600 dark:text-white">
+                      Owner at Her Company Inc.
+                    </div>
+                    <div className="text-gray-500 dark:text-white text-xs">
+                      March 2020 - Now
+                    </div>
+                  </li>
+                  <li>
+                    <div className="text-teal-600 dark:text-white">
+                      Owner at Her Company Inc.
+                    </div>
+                    <div className="text-gray-500 dark:text-white text-xs">
+                      March 2020 - Now
+                    </div>
+                  </li>
+                  <li>
+                    <div className="text-teal-600 dark:text-white">
+                      Owner at Her Company Inc.
+                    </div>
+                    <div className="text-gray-500 dark:text-white text-xs">
+                      March 2020 - Now
+                    </div>
+                  </li>
+                  <li>
+                    <div className="text-teal-600 dark:text-white">
+                      Owner at Her Company Inc.
+                    </div>
+                    <div className="text-gray-500 text-xs dark:text-white">
+                      March 2020 - Now
+                    </div>
+                  </li>
+                </ul>
+              </div>
+              <div>
+                <div className="flex items-center space-x-2 w-2/3 justify-between font-semibold dark:text-white text-gray-900 leading-8 mb-3">
+                  <div className="flex items-center gap-x-2">
+                    <span className="text-blue-500">
+                      <svg
+                        className="h-5"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path d="M12 14l9-5-9-5-9 5 9 5z" />
+                        <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                        />
+                      </svg>
+                    </span>
+                    <span className="tracking-wide">Education</span>
+                  </div>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="w-5 h-5"
+                  >
+                    <path d="M5.433 13.917l1.262-3.155A4 4 0 017.58 9.42l6.92-6.918a2.121 2.121 0 013 3l-6.92 6.918c-.383.383-.84.685-1.343.886l-3.154 1.262a.5.5 0 01-.65-.65z" />
+                    <path d="M3.5 5.75c0-.69.56-1.25 1.25-1.25H10A.75.75 0 0010 3H4.75A2.75 2.75 0 002 5.75v9.5A2.75 2.75 0 004.75 18h9.5A2.75 2.75 0 0017 15.25V10a.75.75 0 00-1.5 0v5.25c0 .69-.56 1.25-1.25 1.25h-9.5c-.69 0-1.25-.56-1.25-1.25v-9.5z" />
+                  </svg>
+                </div>
+                <ul className="list-inside space-y-2">
+                  <li>
+                    <div className="text-teal-600 dark:text-white">
+                      Masters Degree in Oxford
+                    </div>
+                    <div className="text-gray-500 dark:text-white text-xs">
+                      March 2020 - Now
+                    </div>
+                  </li>
+                  <li>
+                    <div className="text-teal-600 dark:text-white">
+                      Bachelors in LPU
+                    </div>
+                    <div className="text-gray-500 dark:text-white text-xs">
+                      March 2020 - Now
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            {/* <!-- End of Experience and education grid --> */}
+          </div>
+          <div className="bg-white dark:bg-slate-800 p-3 pb-8 shadow-sm rounded-sm mt-4">
+            <div className="flex items-center justify-between w-2/3 space-x-2 font-semibold dark:text-white text-gray-900 leading-8 mb-3">
+              <div className="flex items-center gap-x-2">
+                <span className="text-blue-500">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={2}
+                    stroke="currentColor"
+                    className="w-5 h-5"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
+                    />
+                  </svg>
+                </span>
+                <span className="tracking-wide">Notification Preferences</span>
+              </div>
+            </div>
+            <form className="mt-4 w-2/3 flex flex-col gap-y-4">
+              <div className="flex justify-between items-center">
+                <p>Email Notifications</p>
+                <label className="relative inline-flex items-center cursor-pointer">
+                  <input
+                    type="checkbox"
+                    value=""
+                    className="sr-only peer"
+                    checked={false}
+                    onChange={(event) => {
+                      null;
+                    }}
+                  />
+                  <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white dark:peer-checked:after:border-slate-500 after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white dark:after:bg-slate-500 after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-mooylw"></div>
+                </label>
+              </div>
+
+              <div className="flex justify-between items-center">
+                <p>Text Notifications</p>
+                <label className="relative inline-flex items-center cursor-pointer">
+                  <input
+                    type="checkbox"
+                    value=""
+                    className="sr-only peer"
+                    checked={true}
+                    onChange={() => {
+                      null;
+                    }}
+                  />
+                  <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white dark:peer-checked:after:border-slate-500 after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white dark:after:bg-slate-500 after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-mooylw"></div>
+                </label>
+              </div>
+            </form>
+          </div>
+          {/* <!-- End of profile tab --> */}
+        </div>
+      </div>
+    </div>
+  );
+}
