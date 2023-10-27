bde3bf36c19e336b7e0d9fd0dd163892bad57318
rayveng1 <rayvengsell@gmail.com>
2023-10-27 06:21:19 -0500

    Added 2 admin pages

---
+++ src/app/dashboard/ScheduleViewer/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+export default function ScheduleViewerPage() {
+  return (
+<div>
+  <h1 style={{ fontSize: '36px', marginLeft: '60px', marginTop: '40px', marginBottom: '-10px'}}>Schedule Viewer</h1>
+  <hr style={{ margin: '0', marginLeft: '60px', marginRight: '60px', marginTop: '30px', marginBottom: '30px'  }} />
+  <div style={{ background: 'green', padding: '20px', marginLeft: '60px', marginRight: '60px'  }}>
+    Viewing ID.Schedule1
+  </div>
+</div>
+
+  );
+}
